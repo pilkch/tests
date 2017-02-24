7b0cfa93ea9ac70b0cd9e5be70fc56530577e224
Chris Pilkington <chris.pilkington@gmail.com>
2017-02-24 23:48:58 +1100

    Added stopwatch and timer example.

---
+++ stopwatch/main.cpp
@@ -0,0 +1,164 @@
+#include <cassert>
+#include <cstdlib>
+#include <cstdio>
+
+#include <unistd.h>
+
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+
+typedef uint64_t durationms_t;
+
+// Get the time since epoch in milliseconds
+durationms_t GetTimeMS()
+{
+  static const std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
+
+  const std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+  return std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count();
+}
+
+void DurationToString(std::ostream& o, durationms_t duration)
+{
+  o<<std::setfill('0')<<std::setw(2)<<((duration / 60000) % 3600)<<":"<<std::setfill('0')<<std::setw(2)<<((duration / 1000) % 60)<<":"<<std::setfill('0')<<std::setw(3)<<(duration % 1000);
+}
+
+
+// ** cStopWatch
+//
+// A stop watch that behaves like a real life stop watch, it has start, stop, reset and a total duration
+
+class cStopWatch
+{
+public:
+  cStopWatch();
+
+  void Start();
+  void Stop();
+  void Reset();
+
+  durationms_t GetTotalDuration() const;
+
+private:
+  durationms_t started;
+  durationms_t totalDuration;
+};
+
+cStopWatch::cStopWatch() :
+  started(0),
+  totalDuration(0)
+{
+}
+
+void cStopWatch::Start()
+{
+  assert(started == 0);
+
+  // Started our stop watch
+  started = GetTimeMS();
+}
+
+void cStopWatch::Stop()
+{
+  // Get the time now
+  const durationms_t now = GetTimeMS();
+
+  // Add the duration for this period
+  totalDuration += now - started;
+
+  // Reset our start time
+  started = 0;
+}
+
+void cStopWatch::Reset()
+{
+  started = 0;
+  totalDuration = 0;
+}
+
+durationms_t cStopWatch::GetTotalDuration() const
+{
+  // Get the time now
+  const durationms_t now = GetTimeMS();
+
+  // Return the previous duration plus the duration of the current period
+  return totalDuration + (now - started);
+}
+
+
+
+
+
+// ** cTimeOut
+//
+// For use in a loop for example to work out when we something has expired
+
+class cTimeOut {
+public:
+  explicit cTimeOut(durationms_t timeout);
+
+  void Reset();
+
+  bool IsExpired() const;
+
+  durationms_t GetRemainingMS() const;
+
+private:
+  const durationms_t timeout;
+  durationms_t startTime;
+};
+
+cTimeOut::cTimeOut(durationms_t _timeout) :
+  timeout(_timeout),
+  startTime(GetTimeMS())
+{
+}
+
+void cTimeOut::Reset()
+{
+  startTime = GetTimeMS();
+}
+
+bool cTimeOut::IsExpired() const
+{
+  return ((GetTimeMS() - startTime) > timeout);
+}
+
+durationms_t cTimeOut::GetRemainingMS() const
+{
+  const int64_t duration = int64_t(GetTimeMS()) - int64_t(startTime);
+  const int64_t remaining = (int64_t(timeout) - duration);
+  return (remaining >= 0) ? remaining : 0;
+}
+
+
+int main(int argc, char* argv[])
+{
+  // Just a little test of the stop watch and timeout
+  cStopWatch stopWatch;
+
+  cTimeOut timeout(5000);
+
+  for (size_t i = 0; i < 3; i++) {
+    timeout.Reset();
+
+    stopWatch.Start();
+
+    while (!timeout.IsExpired()) {
+      std::cout<<"Stop watch time: ";
+      DurationToString(std::cout, stopWatch.GetTotalDuration());
+      std::cout<<", Timeout: "<<(timeout.IsExpired() ? "expired" : "not expired")<<", remaining: ";
+      DurationToString(std::cout, timeout.GetRemainingMS());
+      std::cout<<std::endl;
+
+      const uint64_t timeout_ms = 100;
+      ::usleep(1000 * timeout_ms);
+    }
+
+    stopWatch.Stop();
+  }
+
+  return EXIT_SUCCESS;
+}
